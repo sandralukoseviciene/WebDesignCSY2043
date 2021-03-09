ff643fc1a6fa9c299f9405638b76aa9a90e2e1b2
Sandra_Lukoseviciene <sandralukoseviciene9@gmail.com>
2021-03-09 14:40:20 +0000

    ex 6 updated

--- java/script.js
+++ java/script.js
@@ -1,11 +1,44 @@
 
-function print5() {
-    alert(1);
-    alert(2);
-    alert(3);
-    alert(4);
-    alert(5);
+/*function print5() {
+   console.log(1);
+   console.log(2);
+   console.log(3);
+   console.log(4);
+   console.log(5);
+
 }
 
 print5();
-print5();
+print5(); 
+function myClickFunction() {
+    var element = document.getElementById('pageheading');
+ element.firstChild.nodeValue = 'New Heading';
+
+ var element = document.getElementById('paragraph');
+ element.firstChild.nodeValue = 'New paragraph text';
+}
+
+function myLoadFunction() {
+    var element = document.getElementById('pageheading');
+    element.addEventListener('click' , myClickFunction);
+}
+
+document.addEventListener('DOMContentLoaded' , myLoadFunction);
+*/
+
+function updateParagraph() {
+    var element = document.getElementById('paragraph');
+    element.firstChild.nodeValue = 'New Heading';
+}
+
+function updateH1() {
+    var element = document.getElementById('pageheading');
+    element.firstChild.nodeValue = 'New paragraph contents';
+}
+
+function myLoadFunction() {
+    var element = document.getElementById('paragraph');
+    element.addEventListener('click', updateParagraph);
+}
+
+document.addEventListener('DOMContentLoaded', myLoadFunction);
