68b3db183f8f7ffdb0e9d753c2b5062fd7ad7f2e
Sandra_Lukoseviciene <sandralukoseviciene9@gmail.com>
2021-03-10 09:23:16 +0000

    move random

--- week7/script.js
+++ week7/script.js
@@ -13,7 +13,7 @@ function myLoadFunction() {
 document.addEventListener('DOMContentLoaded', myLoadFunction);
 */
 
-
+/*
 function myClickEvent () {
     var element = document.getElementById('circle');
     var circleOpacity = parseFloat(element.style.opacity);
@@ -26,5 +26,54 @@ function myLoadEvent() {
     element.addEventListener('click', myClickEvent);
     element.style.opacity = 0.5;
 }
+
+document.addEventListener('DOMContentLoaded', myLoadEvent);
+*/
+
+function moveUp() {
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = positionTop -1 + 'px';
+}
+
+function moveDown() {
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = positionTop + 1 + 'px';
+}
+
+function moveLeft() {
+    var element = document.getElementById('circle');
+    var positionLeft = element.offsetLeft;
+    element.style.left = positionLeft - 1 + 'px';
+}
+
+function moveRight() {
+    var element = document.getElementById('circle');
+    var positionLeft = element.offsetLeft;
+    element.style.left = positionLeft + 1 + 'px';
+
+}
+
+function myKeyDown(event) {
+    if (event.keyCode == 37) {
+        setInterval(moveLeft, 10);
+    }
+    if (event.keyCode == 38) {
+        setInterval(moveUp, 10);
+    }
+    if (event.keyCode == 40) {
+        setInterval(moveDown, 10);
+    }
+    if (event.keyCode == 39) {
+        setInterval(moveRight, 10);
+    }
+    
+}
+
+function myLoadEvent() {
+    document.addEventListener('keydown', myKeyDown);
+}
+
 
 document.addEventListener('DOMContentLoaded', myLoadEvent);
