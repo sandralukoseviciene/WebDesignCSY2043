a5d6767257f2a777bba72e8a18cdf7ab04adc130
Sandra_Lukoseviciene <sandralukoseviciene9@gmail.com>
2021-03-22 14:16:08 +0000

    week 8

---
+++ week8/Ex1/example.js
@@ -0,0 +1,28 @@
+var element = document.getElementById('myChart'); //.getContext('2d');
+
+// The type of chart we want to create
+var chartType = "bar";
+
+// The data for our dataset
+var chartData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 2)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }
+        ]
+    };
+
+// Configuration options go here
+var chartOptions = {};
+
+var param = {
+                type:chartType, 
+                data: chartData, 
+                options: chartOptions
+            };
+
+var chart = new Chart(element, param);
